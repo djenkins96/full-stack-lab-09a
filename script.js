e3e7b06a05efa86612048e954e3eaddfae703cf1
djenkins96 <derrick.jenkins@me.com>
2017-06-13 18:46:43 -0400

    finished lab

---
+++ script.js
@@ -0,0 +1,38 @@
+var diceArray = [];
+document.getElementById('generate-button').addEventListener('click', function () {
+    var d = new Die();
+    diceArray.push(d);
+});
+var rollBtn = document.getElementById('roll-button');
+rollBtn.addEventListener('click', function () {
+    for (var i = 0; i < diceArray.length; i++) {
+        diceArray[i].roll();
+    }
+});
+document.getElementById('sum-button').addEventListener('click', function () {
+    var sum = 0;
+    for (var i = 0; i < diceArray.length; i++) {
+        sum = sum + diceArray[i].value;
+    }
+    alert('the sum of the dice is ' + sum);
+});
+var Die = (function () {
+    function Die() {
+        this.div = document.createElement('div');
+        this.div.className = 'die';
+        this.div.addEventListener('click', this.roll.bind(this));
+        this.div.addEventListener('dblclick', this.removeDie.bind(this));
+        this.roll();
+        document.body.appendChild(this.div);
+    }
+    Die.prototype.roll = function () {
+        this.value = Math.ceil(Math.random() * 6);
+        this.div.innerHTML = String(this.value);
+    };
+    Die.prototype.removeDie = function () {
+        this.div.remove();
+        var index = diceArray.indexOf(this);
+        diceArray.splice(index, 1);
+    };
+    return Die;
+}());
